e5decdb802b3e42bf808b9297e19b6538b87369f
Xavier21060 <xaviersan2106@gmail.com>
2022-05-02 20:56:11 -0500

    Trayendome la carpeta sofwaredeEstudinates

--- SofwareDeEstudiantes/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        NotasAlumnos notasAlumnos = new NotasAlumnos();
-        notasAlumnos.sofware();
-        notasAlumnos.alumnosAprobados();
-
-
-    }
-}
